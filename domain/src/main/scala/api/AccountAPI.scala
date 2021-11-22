5ebc6e9024f66740c6e5ffe195cbdee2e99aea1a
BoulayClément <boulayclement@localhost.localdomain>
2021-11-22 18:01:00 +0100

    Exercice 4

--- domain/src/main/scala/api/AccountAPI.scala
+++ domain/src/main/scala/api/AccountAPI.scala
@@ -5,6 +5,11 @@ import model.Account
 
 final class AccountAPI() {
 
-  def createAccount(name: String): Either[String, Account] = ???
+  def createAccount(name: String): Either[String, Account] = {
+    for {
+      _ <- Either.cond(name.length < 15, (), "Name length is superior to 15")
+      _ <- Either.cond(!name.isBlank, (), "Name is blank")
+    } yield {Account(name)}
+  }
 
 }
